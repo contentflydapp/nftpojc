9b87e364d3abc416099f4760d840bf688ec3610f
Henry Chan <henry@kinwo.net>
2022-03-02 17:13:46 +1100

    [CI] Add cmd exit status check on upgradenft
    * when dfx cmd fails, it should fail the build too

--- ciscripts/upgradenft.sh
+++ ciscripts/upgradenft.sh
@@ -1,40 +1,35 @@
-#!/bin/sh -l
+#!/bin/bash
 
 # Upgrade Canisters
 DFX_IDENTITY_PRINCIPAL=$(dfx identity get-principal)
 TOKEN_ID="CFJ"
 TOKEN_NAME="Content Fly PoJC NFT"
-
 CANISTER_CAP_ID="$CANISTER_CAP_ID"
-CANISTER_CAP_LOCAL_ID="$CANISTER_CAP_LOCAL_ID"
 
 printf "Deploying the CF PoJC NFT Canister\n"
 printf "TOKEN_ID=$TOKEN_ID\n"
 printf "TOKEN_NAME=$TOKEN_NAME\n"
 printf "DFX_IDENTITY_PRINCIPAL=$DFX_IDENTITY_PRINCIPAL\n"
+printf "CANISTER_CAP_ID=$CANISTER_CAP_ID\n"
+printf "\n\n"
 
-upgradeicdev() {
-  printf "CANISTER_CAP_ID=$CANISTER_CAP_ID\n"
-  printf "\n\n"
+trap checkExitStatus EXIT
+checkExitStatus() {
+  existStatus=$?
 
-  dfx build --network ic nft
-  dfx canister  --network ic install nft --mode upgrade --argument "(principal \"$DFX_IDENTITY_PRINCIPAL\", \"$TOKEN_ID\", \"$TOKEN_NAME\", principal \"$CANISTER_CAP_ID\", record {
-    logo_type=\"image/jpg\";
-    data=\"\";
-  })"
+  if [ $existStatus -ne 0 ] 
+  then
+    echo "Cmd error: "
+    echo $existStatus
+    exit $existStatus
+  else
+    echo "Cmd executed successfully"
+  fi
 }
 
-upgradelocal() {
-  printf "CANISTER_CAP_LOCAL_ID=$CANISTER_CAP_LOCAL_ID\n"
-  printf "\n\n"
+dfx build --network ic nft
+dfx canister  --network ic install nft --mode upgrade --argument "(principal \"$DFX_IDENTITY_PRINCIPAL\", \"$TOKEN_ID\", \"$TOKEN_NAME\", principal \"$CANISTER_CAP_ID\", record {
+  logo_type=\"image/jpg\";
+  data=\"\";
+})"
 
-  dfx build nft
-  dfx canister  install nft --mode upgrade --argument "(principal \"$DFX_IDENTITY_PRINCIPAL\", \"$TOKEN_ID\", \"$TOKEN_NAME\", principal \"$CANISTER_CAP_LOCAL_ID\", record {
-    logo_type=\"image/jpg\";
-    data=\"\";
-  })"
-}
-
-upgradeicdev
-
-exit 0
